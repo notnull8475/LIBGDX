94111d09050ad6c4699e845d23e3a3be5a83f87c
Dinar Gizzatulin <dinar-arsk@yandex.ru>
2022-08-13 09:01:25 +0300

    hw1

--- core/src/ru/com/notnull/mygame/MyGdxGame.java
+++ core/src/ru/com/notnull/mygame/MyGdxGame.java
@@ -1,6 +1,8 @@
 package ru.com.notnull.mygame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,18 +10,28 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	int clk;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("admin.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
+
+		float x = Gdx.input.getX()- (img.getWidth() >> 1);
+		float y = Gdx.graphics.getHeight() - Gdx.input.getY() - (img.getHeight() >> 1);
+
+		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+			clk++;
+		}
+
+		Gdx.graphics.setTitle("Clicked " + clk + " tipes!");
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
 		batch.end();
 	}
 	
